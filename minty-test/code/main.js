20efe102555562189813a6d202dcfe84eae61a22
Unknown <jkism332@gmail.com>
2018-07-01 15:24:21 +0200

    tested loading config files

--- minty-test/code/main.js
+++ minty-test/code/main.js
@@ -1,5 +1,9 @@
 const sc0 = new Scene(v(1024, 1024), 
-    {},{main:[]},() => {}, () => {}, 60, 60);
+    {},{main:cfg.bckpreset.main},() => {
+        vport.resize(v(800, 600));
+        bck.main.scale = v(0.5,0.5);
+    }, () => {}, 60, 60);
+
 GAME.onload.set(function() { new Background()
     sc0.load();
 })
