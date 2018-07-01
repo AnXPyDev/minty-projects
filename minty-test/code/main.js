1e7c42a44f3082750882669245622747da6a46ba
Unknown <jkism332@gmail.com>
2018-07-01 15:24:21 +0200

    tested loading config files

--- minty-test/code/main.js
+++ minty-test/code/main.js
@@ -1,13 +1,53 @@
 const sc0 = new Scene(v(1024, 1024), 
-    {},{main:[]},() => {}, () => {}, 60, 60);
-GAME.onload.set(function() { new Background()
+    {main:[[]]},{main:cfg.bckpreset.main},() => {
+        vport.resize(v(512, 512));
+        vport.element.style.cursor = "default";
+        bck.main.scale = v(0.25,0.25);
+    }, () => {}, 60, 60);
+
+GAME.onload.set(function() {
     sc0.load();
 })
 
 def("main", class extends Actor {
     constructor() {
         super(v(), "main");
-
+        this.sprite = new Sprite("player", 1, 0);
+        this.size = v(32,32);
+        this.spd = v();
+        this.speed = 5;
+    }
+    tick() {
+        this.spd.x = function() {
+            if (Key.check("a") && Key.check("d")) {
+                return 0
+            } else if (Key.check("a")) {
+                return -1;
+            } else if (Key.check("d")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }()
+        this.spd.y = function() {
+            if (Key.check("w") && Key.check("s")) {
+                return 0
+            } else if (Key.check("w")) {
+                return -1;
+            } else if (Key.check("s")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }()
+        this.pos.x += this.spd.x * this.speed;
+        this.pos.y += this.spd.y * this.speed;
+        bck.main.scale.x = bck.main.scale.y = Math.abs(Math.sin(new Date() / 1000)) + 0.2;
+        this.angle.between(this.pos, Mouse);
+        this.sprite.update();
+    }
+    draw() {
+        //this.sprite.draw(this.pos, this.size, this.angle);
     }
 })
 
