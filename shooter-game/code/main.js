3375ef65905110e52bb05447973a88552b09b5be
Unknown <jkism332@gmail.com>
2018-07-05 15:09:51 +0200

    working collision model

--- shooter-game/code/main.js
+++ shooter-game/code/main.js
@@ -1,9 +1,9 @@
 const s0 = new Scene(v(1024,1024),
 {
-    box:[[v()],[v(32,32)],[v(64,64)]],
+    box:[[v()],[v(32,32)],[v(64,64)],[v(0,64)]],
     player:[[]]
 }, {
-    main:[["bck_glass"], "tiled"]
+    main:[["bck_wood"], "tiled"]
 }, () => {
     bck.main.setScale(v(2,2));
 }, () => {}, 60,60);
@@ -30,12 +30,12 @@ def("box", class extends Actor {
 
 def("player", class extends Actor {
     constructor() {
-        super(v(), "player");
-        this.size = v(32,32);
+        super(v(-100,0), "player");
+        this.size = v(32, 32);
         this.mask = new Polygon();
         this.mask.set([[-1,-1],[1,-1],[1,1],[-1,1]]);
         this.sprite = new Sprite(["box"], 1, 0);
-        this.speed = 5;
+        this.speed = 1;
         this.spd = v();
     }
     tick() {
@@ -62,13 +62,33 @@ def("player", class extends Actor {
             }
 
         }()
-        
-        
 
+        
+        if(collides(this, Instance.filter(["solid"]), v(this.pos.x + this.spd.x * this.speed, this.pos.y)).is) {
+            while(!collides(this, Instance.filter(["solid"]), v(this.pos.x + Math.sign(this.spd.x), this.pos.y)).is) {
+                this.pos.x += Math.sign(this.spd.x);
+            }
+            this.spd.x = 0;
+        }
+
+        if(collides(this, Instance.filter(["solid"]), v(this.pos.x, this.pos.y + this.spd.y * this.speed)).is) {
+            while(!collides(this, Instance.filter(["solid"]), v(this.pos.x, this.pos.y + Math.sign(this.spd.y))).is) {
+                this.pos.y += Math.sign(this.spd.y);
+            }
+            this.spd.y = 0;
+        }
+
+        if(this.spd.x != 0 || this.spd.y != 0) {
+            let off = Random.int(-1, 1);
+            console.log(off)
+            bck.main.setOffset(v(off,off));
+        } else {
+            bck.main.setOffset(v());
+        }
 
         this.pos.x += this.spd.x * this.speed;
         this.pos.y += this.spd.y * this.speed;
-            
+    
         this.sprite.update();
     }
     draw() {
