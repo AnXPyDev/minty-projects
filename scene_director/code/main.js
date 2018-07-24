cc35d7230148691bf669d91e25fe20b9ee555db8
Unknown <jkism332@gmail.com>
2018-07-24 19:23:42 +0200

    Drastically improved scene director

--- scene_director/code/main.js
+++ scene_director/code/main.js
@@ -4,9 +4,9 @@ const s0 = new Scene("s0", v(1024,1024),
     cursor:[[]]
 },
 {
-    white:[["noimage"], "solid", "lightgreen"],
     grid:[["grid"], "tiled"]
 }, () => {
+    bck.grid.setOffset(v(16,16));
 }, () => {
 
 }, 60,60)
@@ -21,8 +21,9 @@ const info = {
     size: v(32,32)
 }
 const colors = {};
-const gridsize = v(32,32);
+let gridsize = v(32,32);
 
+let steps = [];
 
 function setInfo(name, size, gs = size) {
     info.name = name;
@@ -32,7 +33,8 @@ function setInfo(name, size, gs = size) {
         colors[name] = `rgb(${Random.int(0,255)},${Random.int(0,255)},${Random.int(0,255)})`;
     }
 
-    bck.grid.setScale(v(gs.x / 32, gs.y / 32));
+    bck.grid.setScale(v(gs.x / 32, gs.y / 32)); 
+    gridsize = gs;
 }
 
 def("main_logic", class extends Actor {
@@ -40,8 +42,24 @@ def("main_logic", class extends Actor {
         super(v(), "main_logic");
         this.isCollidable = false;
     }
+    tick() {
+        camera.pos.x += (-Key.check("arrowleft") + Key.check("arrowright")) * 8;
+        camera.pos.y += (-Key.check("arrowup") + Key.check("arrowdown")) * 8;
+        when(Key.check("control") && Key.check("z"), () => {
+            Instance.destroy("block", steps[steps.length - 1]);
+            steps.splice(-1, 1);
+        })
+        when(Key.check("control") && Key.check("c"), () => {
+            camera.pos.x = 0;
+            camera.pos.y = 0;
+        })
+    }
     mousedown() {
-        Instance.spawn("block", [v(Math.floor(Mouse.x / gridsize.x) * gridsize.x + gridsize.x / 2, Math.floor(Mouse.y / gridsize.y) * gridsize.y + gridsize.y / 2), info.size]);
+        steps.push(Instance.spawn("block", [v(Math.floor((Mouse.x - gridsize.x / 2) / gridsize.x + 1) * gridsize.x, Math.floor((Mouse.y - gridsize.y / 2 + 1) / gridsize.y + 1) * gridsize.y), info.size]));
+    }
+    draw() {
+        Draw.line(v(-vport.size.x / 2 + camera.pos.x, 0), v(vport.size.x / 2 + camera.pos.x, 0),"lime", 4);
+        Draw.line(v(0, -vport.size.y / 2 + camera.pos.y), v(0, vport.size.y / 2 + camera.pos.y),"lime", 4);
     }
 });
 
@@ -64,14 +82,13 @@ def("block", class extends Actor {
 def("cursor", class extends Actor {
     constructor() {
         super(Mouse, "cursor");
-        this.sprite = new Sprite(["cursor"], 1, 0);
         this.depth = 1000;
         this.size = v(8,8);
     }
-    tick() {
-        this.sprite.update();
-    }
     draw() {
-        this.sprite.draw(this.pos, this.size)
+        Draw.line(v(-vport.size.x + Mouse.x, Mouse.y),v(vport.size.x + Mouse.x, Mouse.y), "white", 2);
+        Draw.line(v(Mouse.x, -vport.size.y + Mouse.y),v(Mouse.x, vport.size.y + Mouse.y), "white", 2);
+        Draw.text(`${Mouse.x}, ${Mouse.y}`, v(Mouse.x + 64, Mouse.y + 40), "white", 16);
+        Draw.text(`${Math.floor((Mouse.x - gridsize.x / 2) / gridsize.x + 1)}, ${Math.floor((Mouse.y - gridsize.y / 2) / gridsize.y + 1) }`, v(Mouse.x + 64, Mouse.y + 80), "white", 16);
     }
 })
